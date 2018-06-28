2d590c52dddaf10455427df7fab940e4c0bd8c9c
Leonardo (NPDS) <leonardo.dutra@crateus.ufc.br>
2018-06-28 09:41:20 -0300

    #1 - implementada SeparateChaining Hash

--- src/app/eda/App.java
+++ src/app/eda/App.java
@@ -1,9 +1,26 @@
 package app.eda;
 
+import java.util.Scanner;
+
+import app.eda.hash.SeparateChainingHashST;
+
 public class App {
 
 	public static void main(String[] a) {
-		System.out.println("Inicializado!" + 100%8);
+		System.out.println("Inicializado...");
+		System.out.println("Testando HASH");
+		System.out.println("1. Testando Encadeamento Separado");
+		
+		SeparateChainingHashST<String, Integer> st = new SeparateChainingHashST<String, Integer>(5);
+		Scanner in = new Scanner(System.in);
+		
+		
+			for(int i = 0; in.hasNext(); i++) {
+				System.out.println("Digite algo para entrada");
+				String key = in.nextLine();
+				st.put(key, i);
+			}
+		in.close();
 	}
 	
 }
---
+++ src/app/eda/hash/SeparateChainingHashST.java
@@ -0,0 +1,102 @@
+package app.eda.hash;
+
+import app.eda.st.ST;
+
+public class SeparateChainingHashST<K,V> implements ST<K,V>{
+
+	private int m;
+	private Node[] vector;
+	private int n; // número de pares chaves-valores
+	
+	private static class Node{
+		private Object key;
+		private Object value;
+		private Node next;
+		
+		public Node(Object key, Object value, Node next) {
+			this.key = key;
+			this.value = value;
+			this.next = next;
+		}
+	}
+	
+	public SeparateChainingHashST(int m) {
+		this.m = m;
+		vector = new Node[m];
+	}
+	
+	private int hash(K key) {
+		return (key.hashCode() & 0x7fffffff) % m;
+	}
+	
+	@Override
+	public void put(K key, V value) {
+		if(value == null) {
+			delete(key);
+			return;
+		}
+		
+		int i = hash(key);
+		for(Node x = vector[i]; x != null; x = x.next) {
+			if(key.equals(x.key)) {
+				x.value = value;
+				return;
+			}
+		}
+		n++;
+		vector[i] = new Node(key, value, vector[i]);
+		System.out.println("Item add into list");
+	}
+
+	@Override
+	public V get(K key) {
+		int i = hash(key);
+		for(Node x = vector[i]; x != null; x = x.next) {
+			if(key.equals(x.key))
+				return (V)x.value;
+		}
+		return null;
+	}
+
+	@Override
+	public boolean contains(K key) {
+		return get(key) != null;
+	}
+
+	@Override
+	public void delete(K key) {
+		if(key == null) throw new IllegalArgumentException("Valor nulo para deletar");
+		
+		if(get(key) != null) {
+			n--;
+			
+		}
+	}
+
+	private Node delete(Node x, K key) {
+		if(x == null) return null;
+		if(key.equals(x.key)) {
+			n--;
+			return x.next;
+		}
+		x.next = delete(x.next, key);
+		return x;
+	}
+	
+	@Override
+	public boolean isEmpty() {
+		return size() == 0;
+	}
+
+	@Override
+	public int size() {
+		return n;
+	}
+
+	@Override
+	public Iterable<K> keys() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
